a749d0231415c4c0a687d3d839e495c08fc7fd2f
respag <respag@hotmail.com>
2014-09-03 18:33:32 -0500

    initial project version

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = 0;
+            int x = 12;
+            Cuadrado(x, ref res);
+            Console.WriteLine("El cuadrado de {0} es {1}", x, res);
+        }
+
+        private static void Cuadrado(int x, ref int res)
+        {
+            res = x * x;
+        }
+    }
+}
